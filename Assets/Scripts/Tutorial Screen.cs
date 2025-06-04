7dd06ecce5e41570753d55d8d3f1b8adb3b4d9a0
Kreiglist <aldyap17403@gmail.com>
2025-06-05 00:38:46 +0700

    Push 4-5/6/2025

--- Assets/Scripts/Tutorial Screen.cs
+++ Assets/Scripts/Tutorial Screen.cs
@@ -1,11 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TutorialScreen : MonoBehaviour
-{
-    // Start is called before the first frame update
+{ 
+    [SerializeField] private Image tutorialImage;
+    [SerializeField] private Sprite[] tutorialSprites;
+    
+    private int currentTutorialIndex = 0;
     void Start()
     {
-        
+        UpdateTutorial();
+    }
+    public void FlipToNext()
+    {
+        if (currentTutorialIndex < tutorialSprites.Length - 1)
+        {
+            currentTutorialIndex++;
+            UpdateTutorial();
+        }
+    }
+    public void FlipToPrevious()
+    {
+        if (currentTutorialIndex > 1)
+        {
+            currentTutorialIndex--;
+            UpdateTutorial();
+        }
+    }
+    private void UpdateTutorial()
+    {
+        tutorialImage.sprite = tutorialSprites[currentTutorialIndex];
     }
 }
